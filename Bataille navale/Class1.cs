ee409ffd0d8d8d993fcad14f93839765e7c5cea6
adrienphan <adrienphancaophai@gmail.com>
2021-12-21 14:05:19 +0100

    placement des bateaux rough

---
+++ Bataille navale/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bataille_navale
+{
+    internal class GameMethods
+    {
+        public static int lineCharacterToInt(char charToUnicode)
+        {
+            return (int)charToUnicode - (int)'A';
+        }
+        
+    }
+}
--- Bataille navale/Program.cs
+++ Bataille navale/Program.cs
@@ -16,10 +16,7 @@ namespace ConsoleApplication1
         {
             GameLoop();
         }
-        static int lineCharacterToInt(char charToUnicode)
-        {
-            return (int)charToUnicode - (int)'A';
-        }
+        
 
         public static async Task GameLoop()
         {
@@ -37,23 +34,23 @@ namespace ConsoleApplication1
             longitudeShot = 0;
             latitudeShot = 0;
 
-            battleMap[1, 1] = "carrier";
-            battleMap[1, 2] = "carrier";
-            battleMap[1, 3] = "carrier";
-            battleMap[1, 4] = "carrier";
-            battleMap[1, 5] = "carrier";
-            battleMap[2, 7] = "battleship";
-            battleMap[3, 7] = "battleship";
-            battleMap[4, 7] = "battleship";
-            battleMap[5, 7] = "battleship";
-            battleMap[3, 2] = "cruiser";
-            battleMap[4, 2] = "cruiser";
-            battleMap[5, 2] = "cruiser";
-            battleMap[5, 4] = "submarine";
-            battleMap[6, 4] = "submarine";
-            battleMap[7, 4] = "submarine";
-            battleMap[8, 8] = "destroyer";
-            battleMap[8, 9] = "destroyer";
+            //battleMap[1, 1] = "carrier";
+            //battleMap[1, 2] = "carrier";
+            //battleMap[1, 3] = "carrier";
+            //battleMap[1, 4] = "carrier";
+            //battleMap[1, 5] = "carrier";
+            //battleMap[2, 7] = "battleship";
+            //battleMap[3, 7] = "battleship";
+            //battleMap[4, 7] = "battleship";
+            //battleMap[5, 7] = "battleship";
+            //battleMap[3, 2] = "cruiser";
+            //battleMap[4, 2] = "cruiser";
+            //battleMap[5, 2] = "cruiser";
+            //battleMap[5, 4] = "submarine";
+            //battleMap[6, 4] = "submarine";
+            //battleMap[7, 4] = "submarine";
+            //battleMap[8, 8] = "destroyer";
+            //battleMap[8, 9] = "destroyer";
 
            
 
@@ -81,23 +78,21 @@ namespace ConsoleApplication1
                     // Verifier la validité de la ligne
                     if (longitudeCheck.IsMatch(linePosition))
                     {
-                        longitudeShot = lineCharacterToInt(playerInput[0]);
                          isLineValid = true;
                     }
                     else
                     {
-                        throw new Exception("La lettre de la ligne n'est pas valide");
+                        throw new Exception("La lettre de la ligne n'est pas valide, réessayez :");
                     }
                     // Verifier la validité de la colonne
                     if (latitudeCheck.IsMatch(columnPosition)
                         && int.Parse(columnPosition) > 0 && int.Parse(columnPosition) <= 10)
                     {
-                        latitudeShot = int.Parse(columnPosition) - 1;
                         isColumnValid = true;
                     }
                     else
                     {
-                        throw new Exception("Le chiffre de la colonne n'est pas valide");
+                        throw new Exception("Le chiffre de la colonne n'est pas valide, réessayez :");
                     }
                     if (isLineValid && isColumnValid)
                     {
@@ -117,15 +112,69 @@ namespace ConsoleApplication1
              */
             void PlaceBoat(string boat, int boatLength)
             {
-                Console.WriteLine($"Placez votre {boat} (coordonnée la plus haute ou a gauche).");
-                //coordonnée du départ du bateau :
                 string baseCoord;
-                //confirmer l'entrée.
-                bool isCoordCorrect = false;
-                while (!isCoordCorrect)
+                
+                bool isBoatCorrect = false;
+                string boatBaseX = "";
+                string boatBaseY = "";
+                int boatBoxX = 0;
+                int boatBoxY = 0;
+                while (!isBoatCorrect)
                 {
-                    baseCoord = Console.ReadLine();
-                    isCoordCorrect = CoordCheck(baseCoord);
+                    bool isCoordCorrect = false;
+                    isBoatCorrect = false;
+                    Console.WriteLine($"Placez votre {boat} (coordonnée la plus haute ou a gauche).");
+                    //coordonnée du départ du bateau :
+                    //confirmer l'entrée.
+                    while (!isCoordCorrect)
+                    {
+                        baseCoord = Console.ReadLine();
+                        isCoordCorrect = CoordCheck(baseCoord);
+                        //traduire les coordonnées
+                        boatBaseX = baseCoord.Substring(0, 1/*EXCLU*/);
+                        boatBaseY = baseCoord.Substring(1);
+                    }
+                    //demander vertical ou horizontal
+                    Console.WriteLine("Poursuivre le bateau vers la droite(d) ou vers le bas(b) ?");
+                    string boatDirection = Console.ReadLine();
+                    //checker les emplacements dans le tableau
+                    //Pour chaque case du bateau, si elle est valide, passer a la suivante, sinon arreter
+                    for(int box = 0; box < boatLength; box++)
+                    {
+                        if (boatDirection == "d")
+                        {
+                            boatBoxX = GameMethods.lineCharacterToInt(boatBaseX[0]) + box;
+                            boatBoxY = Convert.ToInt32(boatBaseY) - 1;
+                            if (battleMap[boatBoxX, boatBoxY] != null || boatBoxX >= 10)
+                            {
+                                Console.WriteLine("Coordonnée impossible, un bateau ou le bord de la carte est dans le chemin !");
+
+                                break;
+                            } else if (box == boatLength - 1)
+                            {
+                                isBoatCorrect =true;
+                            }
+                        } else if (boatDirection == "b")
+                        {
+                            boatBoxX = GameMethods.lineCharacterToInt(boatBaseX[0]);
+                            boatBoxY = Convert.ToInt32(boatBaseY) - 1 + box;
+                            if (battleMap[boatBoxX, boatBoxY] != null || boatBoxY >= 10)
+                            {
+                                Console.WriteLine("Coordonnée impossible, un bateau ou le bord de la carte est dans le chemin !");
+
+                                break;
+                            }
+                            else if (box == boatLength - 1)
+                            {
+
+                                isBoatCorrect = true;
+                            }
+                        }
+
+                        //placer les cases du bateau
+                        battleMap[boatBoxX, boatBoxY] = boat;
+                        Console.WriteLine($"Case de {boat} en : {boatBoxX}, {boatBoxY}");
+                    }
                 }
             }
 
@@ -133,7 +182,6 @@ namespace ConsoleApplication1
             {
                 PlaceBoat(boat.Key, boat.Value);
             }
-
 
             /*******************************************************************************/
 
@@ -150,7 +198,7 @@ namespace ConsoleApplication1
                     // Verifier la validité de la ligne
                     if (longitudeCheck.IsMatch(linePosition))
                     {
-                        longitudeShot = lineCharacterToInt(playerInput[0]);
+                        longitudeShot = GameMethods.lineCharacterToInt(playerInput[0]);
                     }
                     else
                     {
@@ -181,7 +229,7 @@ namespace ConsoleApplication1
                 linePosition = playerInput.Substring(0, 1/*EXCLU*/);
                 columnPosition = playerInput.Substring(1);
 
-                longitudeShot = lineCharacterToInt(playerInput[0]);
+                longitudeShot = GameMethods.lineCharacterToInt(playerInput[0]);
                 latitudeShot = int.Parse(columnPosition) - 1;
 
                 // Attaque de l'adversaire
